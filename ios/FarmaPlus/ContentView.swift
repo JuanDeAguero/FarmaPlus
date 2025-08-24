dab7d1123ea16256461c57057cc6af4b1df0bebc
Juan <juanmgdeaguero@gmail.com>
2025-08-24 19:59:06 +0200

    Remove user data

--- ios/FarmaPlus/ContentView.swift
+++ ios/FarmaPlus/ContentView.swift
@@ -1,17 +1,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("FARMA+")
+  var body: some View {
+    TabView {
+      NavigationView {
+        ScrollView {
+          VStack {
+            Button(action: {
+              
+            }) {
+              Text("Tienda")
+                .foregroundColor(.white)
+                .padding()
+            }
+            .background(.green)
+            .cornerRadius(8)
+          }
         }
-        .padding()
+        .navigationTitle("Tienda")
+        .refreshable {
+          await Task.sleep(2 * 1_000_000_000)
+        }
+      }
+      .tabItem {
+        Image(systemName: "cart")
+        Text("Tienda")
+      }
+
+      VStack {
+        Text("Chat")
+      }
+      .tabItem {
+        Image(systemName: "message")
+        Text("Chat")
+      }
+
+      VStack {
+        Text("Usuario")
+      }
+      .tabItem {
+        Image(systemName: "person")
+        Text("Usuario")
+      }
     }
+    .tint(.green)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
